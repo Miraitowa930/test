07bd5501cdd7b5cf9ab9507cc18c9a06c65c9348
Miraitowa930 <1584604443@qq.com>
2020-11-18 12:02:49 +0800

    echarts

---
+++ echarts/my-app/src/app/app.component.ts
@@ -0,0 +1,35 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.styl']
+})
+export class AppComponent implements OnInit {
+
+  // title = 'my-app';
+  option: any;
+  constructor(){
+
+  }
+  // tslint:disable-next-line: typedef
+  ngOnInit(){
+    this.option = {
+      xAxis: {
+        type: 'category',
+        data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
+    },
+    yAxis: {
+        type: 'value'
+    },
+    series: [{
+        data: [120, 200, 150, 80, 70, 110, 130],
+        type: 'bar',
+        showBackground: true,
+        backgroundStyle: {
+            color: 'rgba(220, 220, 220, 0.8)'
+        }
+    }]
+    };
+  }
+}
